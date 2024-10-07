20795279dcd296fcdddf7891d11c34d39300aad0
dentonflake <denton.flake@gmail.com>
2024-10-07 14:39:38 -0700

    removed semicolons and example

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-
-    socket.on('message', (msg) => {
-        console.log('Message received:', msg);
-
-        io.emit('message', `Server says: ${msg}`);
-    });
+    console.log('A user connected:', socket.id)
 
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-    });
-});
+        console.log('User disconnected:', socket.id)
+    })
+})
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`)
+})
